8fea318e6f09af65dab6d47ecfddb97a62505b64
adai2017 <AndrewDai2017@u.northwestern.edu>
2017-02-14 23:46:15 -0600

    Initial rough code for IR modules

---
+++ Module/Module.ino
@@ -0,0 +1,13 @@
+// Code for Regular IR Modules (No Bluetooth)
+//
+// Using Pins 20 (ADC) and 21 (Digital Output)
+
+void setup() {
+  // Initialize Pins
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
