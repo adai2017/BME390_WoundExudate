a9cb9e2040d94bdf233f3756a3738988d2d34d43
adai2017 <AndrewDai2017@u.northwestern.edu>
2017-04-07 13:01:07 -0500

    WIP code
    - Updated error in definitions
    - Included debugging LED
    - Temporarily removed Snooze library commands

--- IRB_Module_Ver2/IRB_Module_Ver2.ino
+++ IRB_Module_Ver2/IRB_Module_Ver2.ino
@@ -5,27 +5,28 @@
 #include <SD.h>
 
 
-#define ONE_1 = 0;      // Binary 1 for ONE's is Pin 0
-#define ONE_2 = 1;      // Binary 2 for ONE's is Pin 1
-#define ONE_4 = 2;      // Binary 4 for ONE's is Pin 2
-#define ONE_8 = 3;      // Binary 8 for ONE's is Pin 3
+#define ONE_1 0      // Binary 1 for ONE's is Pin 0
+#define ONE_2 1      // Binary 2 for ONE's is Pin 1
+#define ONE_4 2      // Binary 4 for ONE's is Pin 2
+#define ONE_8 3      // Binary 8 for ONE's is Pin 3
                         // Pin 4 reserved for SD communication
-#define TEN_1 = 5;      // Binary 1 for TEN's is Pin 5
-#define TEN_2 = 6;      // Binary 2 for TEN's is Pin 6
-#define TEN_4 = 7;      // Binary 4 for TEN's is Pin 7
-#define TEN_8 = 8;      // Binary 8 for TEN's is Pin 8
+#define TEN_1 5      // Binary 1 for TEN's is Pin 5
+#define TEN_2 6      // Binary 2 for TEN's is Pin 6
+#define TEN_4 7      // Binary 4 for TEN's is Pin 7
+#define TEN_8 8      // Binary 8 for TEN's is Pin 8
 
 int ID = 0;             // Initializes global variable "ID" used for patient ID number
 
+int led = 13;
 
 // Teensy set to hibernate mode -- approx IDD = 15 uA
 // USB regulator put into a low power state (not used)
-SnoozeAlarm alarm;
-SnoozeBlock config_teensy36(alarm);   // Configures Snooze to use Alarm drivers
+//SnoozeAlarm alarm;
+//SnoozeBlock config_teensy36(alarm);   // Configures Snooze to use Alarm drivers
 
 void setup() {
   // Initialize Snooze Triggers
-  alarm.setAlarm(0, 30, 0);           // Uses RTC to wakeup after (0 HOURS, 30 MINUTES, 0 SECONDS)
+  //alarm.setAlarm(0, 30, 0);           // Uses RTC to wakeup after (0 HOURS, 30 MINUTES, 0 SECONDS)
 
   // Thumbwheel switch pin setup
   pinMode(ONE_1, INPUT_PULLUP);       // Input mode with pullup resistors
@@ -53,22 +54,33 @@ void setup() {
   int TENS = 1*TEN_1_val + 2*TEN_2_val + 4*TEN_4_val + 8*TEN_8_val;
 
   ID = 10*TENS + ONES;
-
-  // Initialize SD card and SD card library
-  SD.begin();
   String ID_name = String(ID);
 
-  if (!SD.exists(ID_name)) {          // Creates file directory for patient with ID number
-    SD.mkdir(ID_name);
-  }
+//  // Initialize SD card and SD card library
+//  SD.begin();
+
+//
+//  if (!SD.exists(ID_name)) {          // Creates file directory for patient with ID number
+//    SD.mkdir(ID_name);
+//  }
+
+  // Initialize USB
+  Serial.begin(9600);
+
+  pinMode(led, OUTPUT);
+
+  Serial.println(ID_name);
 
 }
 
 void loop() {
-  int who = Snooze.hibernate(config_teensy36);
-
-  if (who == 35)  {                   // Value of 35 is RTC wakeup condition
-    
-  }
-
+//  int who = Snooze.hibernate(config_teensy36);
+//
+//  if (who == 35)  {                   // Value of 35 is RTC wakeup condition
+//    
+//  }
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);               // wait for a second
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);               // wait for a second
 }
