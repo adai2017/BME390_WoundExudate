eb97ad36f97b1e727feb6fbd88e5f4a74e1fbfa6
adai2017 <AndrewDai2017@u.northwestern.edu>
2017-06-11 10:42:48 -0500

    Included additional .mat files from testing

--- MATLAB Testing/WoundExudateAnalysis.m
+++ MATLAB Testing/WoundExudateAnalysis.m
@@ -1,33 +1,78 @@
 clear; clc; close all;
-files = {'WoundExudateTesting_Case_022817_1.mat',...
-         'WoundExudateTesting_Case_022817_2.mat',...
-         'WoundExudateTesting_Case_022817_3.mat',...
-         'WoundExudateTesting_Case_022817_4.mat',...
-         'WoundExudateTesting_Case_022817_5.mat'};
+% files = {'WoundExudateTesting_Case_022817_1.mat',...
+%          'WoundExudateTesting_Case_022817_2.mat',...
+%          'WoundExudateTesting_Case_022817_3.mat',...
+%          'WoundExudateTesting_Case_022817_4.mat',...
+%          'WoundExudateTesting_Case_022817_5.mat'};
 
 % files = {'WoundExudateTesting_Case_030217_1.mat',...
 %          'WoundExudateTesting_Case_030217_2.mat',...
-%          'WoundExudateTesting_Case_030217_3.mat',...
-%          'WoundExudateTesting_Case_030217_1_2.mat'};
+%          'WoundExudateTesting_Case_030217_3.mat'};
+
+% files = {'WoundExudateTesting_None_022817_1.mat',...
+%          'WoundExudateTesting_None_022817_2.mat',...
+%          'WoundExudateTesting_None_022817_3.mat',...
+%          'WoundExudateTesting_None_022817_4.mat',...
+%          'WoundExudateTesting_None_022817_5.mat'};
+
+% files = {'Water_Light2Gamgee_04062017_1.mat',...
+%          'Water_Light2Gamgee_04062017_2.mat',...
+%          'Water_Light2Gamgee_04062017_3.mat',...
+%          'Water_Light2Gamgee_04062017_4.mat',...
+%          'Water_Light2Gamgee_04062017_5.mat',...
+%          'Water_Light2Gamgee_04062017_6.mat'};
+     
+% files = {'Water_15hours_05152017.mat'};
+
+% files = {'Water_Saturation_04272017_2.mat',...
+%          'Water_Saturation_04272017_3.mat',...
+%          'Water_Saturation_04272017_4.mat',...    
+%          'Water_Saturation_04272017_5.mat'};
+
+% files = {'Water_NoSponge_04272017_1.mat'};
+
+%files = {'Water_Light2Gamgee_04062017_5.mat'};
 
 N = length(files);
-Init = zeros(1,N);
-Peak = zeros(1,N);
-Plat = zeros(1,N);
+% Init = zeros(1,N);
+% Peak = zeros(1,N);
+% Plat = zeros(1,N);
 
-plot_init = [];
-plot_plat = [];
+% plot_init = [];
+% plot_plat = [];
 
 hold on
 for ii = 1:N
     load(files{ii});
-    data = squeeze(MeanSet1);
-    n1 = size(data,1);
-    n2 = size(data,2);
-    tempdata = reshape(data, [n1*n2, 1]);
+    data1 = squeeze(MeanSet1);
+%     data2 = squeeze(MeanSet2);
+%     data3 = squeeze(MeanSet3);
+%     data4 = squeeze(MeanSet4);
+    
+    n1 = size(data1,1);
+    n2 = size(data1,2);
+    
+%     nn1 = size(data2,1);
+%     nn2 = size(data2,2);
+%     
+%     nnn1 = size(data1,1);
+%     nnn2 = size(data1,2);
+%     
+%     nnnn1 = size(data1,1);
+%     nnnn2 = size(data1,2);
+    
+    tempdata1 = reshape(data1, [n1*n2, 1]);
+%     tempdata2 = reshape(data2, [nn1*nn2, 1]);
+%     tempdata3 = reshape(data3, [nnn1*nnn2, 1]);
+%     tempdata4 = reshape(data4, [nnnn1*nnnn2, 1]);
 
     figure(1)
-    plot(tempdata)
+    hold on
+    p1 = plot(tempdata1)
+%     p2 = plot(tempdata2)
+%     p3 = plot(tempdata3)
+%     p4 = plot(tempdata4)
+    hold off
 %     Init(ii) = Data(1,1);
 %     Peak(ii) = min(Data);
 %     
@@ -37,26 +82,23 @@ for ii = 1:N
 %     ind_plat = ind_plat(1);
 %     Plat(ii) = Data(ind_plat,1);
 
-
-
-    Init(ii) = mean(data(:,1));
-    Plat(ii) = mean(data(:,end));
+%     Init(ii) = mean(data(:,1));
+%     Plat(ii) = mean(data(:,end));
     
-    plot_init = [plot_init; 1 data(1)];
-    plot_plat = [plot_plat; length(tempdata) data(end)];
+%     plot_init = [plot_init; 1 data(1)];
+%     plot_plat = [plot_plat; length(tempdata) data(end)];
 end
 
-p1 = plot(plot_init(:,1), plot_init(:,2), 'gd', 'MarkerSize', 10, 'LineWidth', 1.5);
-p2 = plot(plot_plat(:,1), plot_plat(:,2), 'rd', 'MarkerSize', 10, 'LineWidth', 1.5);
+% p1 = plot(plot_init(:,1), plot_init(:,2), 'gd', 'MarkerSize', 10, 'LineWidth', 1.5);
+% p2 = plot(plot_plat(:,1), plot_plat(:,2), 'rd', 'MarkerSize', 10, 'LineWidth', 1.5);
 
-hold off
-
-title('IR Voltage Resposne | 4 gamgee layers | Case')
+title('IR Voltage Resposne | 2 Gamgee Layers | No Case')
 xlabel('Time [sec]')
 ylabel('Voltage [V]')
-legend([p1 p2], 'Initial Voltage', 'Plateau Voltage')
+%legend([p1 p2 p3 p4], 'Adjacent Sensor', 'Sensor 2mm away', 'Sensor 4mm away', 'Sensor 6mm away')
+% legend([p1 p2], 'Initial Voltage', 'Plateau Voltage')
 
-Voltage.Initial = Init;
-Voltage.Plateau = Plat;
+% Voltage.Initial = Init;
+% Voltage.Plateau = Plat;
 
-save('CaseStats2.mat', 'Voltage')
+% save('CaseStats2.mat', 'Voltage')
--- MATLAB Testing/WoundExudateTesting.m
+++ MATLAB Testing/WoundExudateTesting.m
@@ -1,27 +1,53 @@
 nScopeAPI('open',1);
-n = 20;         % Number of minutes want to run
+%n = 900;         % Number of minutes want to run
 %interval = 1;   % How long between intervals in minutes
 fs = 5;         % Sampling Rate
-DataSet1 = zeros(60,fs,n);
-MeanSet1 = zeros(60,1,n);
 
-i = 1;
+DataSet1 = [];
+% DataSet2 = [];
+% DataSet3 = [];
+% DataSet4 = [];
+
+MeanSet1 = [];
+% MeanSet2 = [];
+% MeanSet3 = [];
+% MeanSet4 = [];
 
 for i = 1:1:n
     fprintf('Starting Minute %d Testing...\n', i)
-    TempDataSet = nScopeReadCh1(fs, fs*60, 1);
-    TempDataSet = reshape(TempDataSet, [fs, 60]);
-    TempDataSet = TempDataSet';
-    DataSet1(:,:,i) = TempDataSet(:,:);
+%     Temp = nScopeReadCh1Ch2(fs, fs*60, [1, 1]);
+    Temp = nScopeReadAllChannels(fs, fs*60, [1, 1, 1, 1]);
+    
+    TempData1 = Temp(:,1);
+    TempData1 = reshape(TempData1, [fs, 60]);
+    TempData1 = TempData1';
+    DataSet1(:,:,i) = TempData1(:,:);
     MeanSet1(:,:,i) = mean(DataSet1(:,:,i),2);
+    
+%     TempData2 = Temp(:,2);
+%     TempData2 = reshape(TempData2, [fs, 60]);
+%     TempData2 = TempData2';
+%     DataSet2(:,:,i) = TempData2(:,:);
+%     MeanSet2(:,:,i) = mean(DataSet2(:,:,i),2);
+%     
+%     TempData3 = Temp(:,3);
+%     TempData3 = reshape(TempData3, [fs, 60]);
+%     TempData3 = TempData3';
+%     DataSet3(:,:,i) = TempData3(:,:);
+%     MeanSet3(:,:,i) = mean(DataSet3(:,:,i),2);
+%     
+%     TempData4 = Temp(:,4);
+%     TempData4 = reshape(TempData4, [fs, 60]);
+%     TempData4 = TempData4';
+%     DataSet4(:,:,i) = TempData4(:,:);
+%     MeanSet4(:,:,i) = mean(DataSet4(:,:,i),2);
 
-    if i > 2
-        if any(MeanSet1(:,:,i) > MeanSet1(:,:,i - 1))
-            fprintf('STRIKETHROUGH: MINUTE %d\n', i);
-        end
-    end
     fprintf('Completed Minute %d Testing.\n', i)
-    save('WoundExudateTesting_Case_030217_1_2.mat')  % Resaves collected data after every iteration
+%     save('Water_15hours_05152017.mat')  % Resaves collected data after every iteration
+    
+%     if ~isempty(warnmsg)
+%         [warnmsg, msgid] = lastwarn;
+%     end
 end
 
 nScopeAPI('close');
